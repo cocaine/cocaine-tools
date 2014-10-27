9f340d12059bc580000c3923181667db9f6761f0
Anton Tiurin <noxiouz@yandex.ru>
2014-10-27 19:37:05 +0300

    [Test] misc tests

---
+++ tests/test_misc.py
@@ -0,0 +1,39 @@
+#
+# Copyright (c) 2013+ Evgeny Safronov <division494@gmail.com>
+# Copyright (c) 2013+ Anton Tiurin <noxiouz@yandex.ru>
+# Copyright (c) 2011-2014 Other contributors as noted in the AUTHORS file.
+#
+# This file is part of Cocaine-tools.
+#
+# Cocaine is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Cocaine is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+import tarfile
+
+from cocaine.tools.actions import readArchive
+from cocaine.tools.actions import CocaineConfigReader
+
+from nose import tools
+
+
+@tools.raises(tarfile.TarError)
+def test_read_archive():
+    readArchive(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                             "fixtures/simple_app/manifest.json"))
+
+
+def test_config_reader():
+    CocaineConfigReader.load(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                             "fixtures/simple_app/manifest.json"))
