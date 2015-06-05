5954532e7c0d82e95a291cd1555e72a3cd550274
Anton Tiurin <noxiouz@yandex.ru>
2015-06-05 18:45:31 +0300

    Version bump 0.12.0.0-rc32

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ else:
 
 setup(
     name="cocaine-tools",
-    version="0.12.0.0rc28",
+    version="0.12.0.0rc32",
     author="Anton Tyurin",
     author_email="noxiouz@yandex.ru",
     maintainer='Evgeny Safronov',
