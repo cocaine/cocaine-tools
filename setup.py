742ae982dfe12626ed4017b87024f192a37e3580
Anton Tiurin <noxiouz@yandex.ru>
2015-08-19 17:39:55 +0300

    Version bump 0.12.3.5

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ else:
 
 setup(
     name="cocaine-tools",
-    version="0.12.3.3",
+    version="0.12.3.5",
     author="Anton Tyurin",
     author_email="noxiouz@yandex.ru",
     maintainer='Evgeny Safronov',
