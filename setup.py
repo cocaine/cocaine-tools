b53edbdd623ce2548af65d8fb98a59c310d10518
Anton Tiurin <noxiouz@yandex.ru>
2015-09-15 22:24:22 +0300

    Version 0.12.4.3

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ else:
 
 setup(
     name="cocaine-tools",
-    version="0.12.4.2",
+    version="0.12.4.3",
     author="Anton Tyurin",
     author_email="noxiouz@yandex.ru",
     maintainer='Evgeny Safronov',
