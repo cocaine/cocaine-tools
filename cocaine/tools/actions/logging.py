ea340d2a86c5e0566832c460a9e19f4f0e79be0d
antmat <antmat@me.com>
2016-07-27 11:02:00 +0300

    feat(): add support for new logging service

---
+++ cocaine/tools/actions/logging.py
@@ -0,0 +1,104 @@
+#
+# Copyright (c) 2016+ Anton Matveenko <antmat@me.com>
+# Copyright (c) 2013+ Anton Tyurin <noxiouz@yandex.ru>
+# Copyright (c) 2013+ Evgeny Safronov <division494@gmail.com>
+# Copyright (c) 2011-2014 Other contributors as noted in the AUTHORS file.
+#
+# This file is part of Cocaine-tools.
+#
+# Cocaine is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Cocaine is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from collections import defaultdict
+import json
+
+from tornado import gen
+
+from cocaine.tools.error import ToolsError
+
+
+class LoggingConfigurator(object):
+    def __init__(self, logging_service):
+        self.logging_service = logging_service
+
+    @gen.coroutine
+    def execute(self):
+        raise NotImplementedError
+
+
+class LoggingConfigListLoggers(LoggingConfigurator):
+    @gen.coroutine
+    def execute(self):
+        filters = yield (yield self.logging_service.list_loggers()).rx.get()
+        raise gen.Return({"loggers": filters})
+
+
+class LoggingConfigSetFilter(LoggingConfigurator):
+    def __init__(self, logging_service, logger_name, filter_def, ttl):
+        super(LoggingConfigSetFilter, self).__init__(logging_service)
+        if not logger_name:
+            raise ToolsError("Logger name is required")
+        if not filter_def:
+            raise ToolsError("filter definition is required")
+        if not ttl:
+            raise ToolsError("filter TTL is required")
+        self.name = logger_name
+        try:
+            self.filter_def = json.loads(filter_def)
+        except:
+            raise ToolsError("Filter definition is not parsable. It should be JSON array with at least one element")
+        try:
+            self.ttl = int(ttl)
+        except:
+            raise ToolsError("TTL should be numeric")
+        if not isinstance(self.filter_def, (tuple, list)) or len(self.filter_def) == 0:
+            raise ToolsError("Filter definition should be array and contain at least one element")
+
+    @gen.coroutine
+    def execute(self):
+        filters = yield (yield self.logging_service.set_filter(self.name, self.filter_def, self.ttl)).rx.get()
+        raise gen.Return(filters)
+
+
+class LoggingConfigRemoveFilter(LoggingConfigurator):
+    def __init__(self, logging_service, filter_id):
+        super(LoggingConfigRemoveFilter, self).__init__(logging_service)
+        if not filter_id:
+            raise ToolsError("Filter id is required")
+        self.filter_id = int(filter_id)
+
+    @gen.coroutine
+    def execute(self):
+        filters = yield self.logging_service.remove_filter(self.filter_id)
+        raise gen.Return(filters)
+
+
+class LoggingConfigListFilters(LoggingConfigurator):
+    @gen.coroutine
+    def execute(self):
+        filters = yield (yield self.logging_service.list_filters()).rx.get()
+        ret = defaultdict(list)
+        for logger_name, filter_def, filter_id, deadline in filters:
+            ret[logger_name].append({"id": filter_id, "deadline_timestamp": deadline, "filter_definition": filter_def})
+        raise gen.Return(ret)
+
+
+class LoggingConfigSetClusterFilter(LoggingConfigSetFilter):
+    def __init__(self, logging_service, logger_name, filter_def, ttl):
+        super(LoggingConfigSetClusterFilter, self).__init__(logging_service, logger_name, filter_def, ttl)
+
+    @gen.coroutine
+    def execute(self):
+        filters = yield self.logging_service.set_cluster_filter(self.name, self.filter_def, self.ttl)
+        raise gen.Return(filters)
