bd7395f94c57bbbbdbf8f424474f295554e54eae
Evgeny Safronov <division494@gmail.com>
2016-11-01 16:43:35 +0300

    Revert "Allow mds keys"

--- cocaine/proxy/mds_direct.py
+++ cocaine/proxy/mds_direct.py
@@ -8,8 +8,6 @@ from random import shuffle
 
 import msgpack
 
-import json
-
 from tornado import gen
 from tornado import httputil
 from tornado.httpclient import AsyncHTTPClient
@@ -33,13 +31,8 @@ from cocaine.proxy.proxy import RESOLVE_TIMEOUT, LOCATORCATEGORY, ESERVICENOTAVA
 
 
 def is_mds_stid(stid):
-    parts = stid.split(".", 2)
-    return len(parts) == 3 and parts[2].startswith('E') and ':' in parts[2]
-
-
-def is_mds_key(key):
-    parts = key.split("/", 1)
-    return len(parts) == 2 and parts[0].isdigit()
+    _, _, tail = stid.split(".", 2)
+    return tail.startswith('E') and ':' in tail
 
 
 class MDSDirect(IPlugin):
@@ -47,7 +40,6 @@ class MDSDirect(IPlugin):
         super(MDSDirect, self).__init__(proxy)
         try:
             self.dist_info_endpoint = config["dist_info_endpoint"]
-            self.mds_dist_info_endpoint = config["mds_dist_info_endpoint"]
             self.locator_port = config["locator_port"]
             self.filter_mds_stid = config.get("filter_stid", True)
             self.service_connect_timeout = timedelta(milliseconds=config.get("service_connect_timeout_ms", 1500))
@@ -61,8 +53,7 @@ class MDSDirect(IPlugin):
 
     def match(self, request):
         if "X-Srw-Key" in request.headers and "X-Srw-Key-Type" in request.headers and "X-Srw-Namespace" in request.headers:
-            key = request.headers["X-Srw-Key"]
-            return not self.filter_mds_stid or is_mds_stid(key) or is_mds_key(key)
+            return not self.filter_mds_stid or is_mds_stid(request.headers["X-Srw-Key"])
         return False
 
     @gen.coroutine
@@ -111,9 +102,6 @@ class MDSDirect(IPlugin):
             raise gen.Return(app)
         raise PluginApplicationError(42, 42, "could not connect to application")
 
-    def is_stid_request(self, request):
-        return request.headers["X-Srw-Key-Type"].upper() == "STID"
-
     @gen.coroutine
     def process(self, request):
         mds_request_headers = httputil.HTTPHeaders()
@@ -129,54 +117,25 @@ class MDSDirect(IPlugin):
         name, event = extract_app_and_event(request)
         timeout = self.proxy.get_timeout(name, event)
         name = self.proxy.resolve_group_to_version(name)
-        if self.is_stid_request(request):
-            url = "%s/gate/dist-info/%s?primary-only" % (self.dist_info_endpoint, key)
-            request.logger.debug("fetching endpoints via mulcagate dist-info - %s", url)
-            srw_request = HTTPRequest(
-                url,
-                method="GET",
-                headers=mds_request_headers,
-                allow_ipv6=True,
-                request_timeout=timeout)
-        else:
-            url = "%s/dist-info-%s/%s" % (self.mds_dist_info_endpoint, request.headers["X-Srw-Namespace"], key)
-            request.logger.debug("fetching endpoints via mds dist-info - %s", url)
-            srw_request = HTTPRequest(
-                url,
-                method="GET",
-                headers=mds_request_headers,
-                allow_ipv6=True,
-                request_timeout=timeout)
+        srw_request = HTTPRequest(
+            "%s/gate/dist-info/%s?primary-only" % (self.dist_info_endpoint, key),
+            method="GET",
+            headers=mds_request_headers,
+            allow_ipv6=True,
+            request_timeout=timeout)
 
-        endpoints = yield self.fetch_mds_endpoints(request, srw_request)
-        locator = Locator(endpoints=endpoints)
-        app = Service(name, locator=locator, timeout=RESOLVE_TIMEOUT)
-        request.logger.info("connecting to app %s", name)
-        app = yield self.reelect_app(request, app)
-        yield self.proxy.process(request, name, app, event, pack_httprequest(request), self.reelect_app, timeout)
-
-    def decode_mulca_dist_info(self, body):
-        lines = body.split("\n")
-        endpoints = [(line.split()[0], self.locator_port) for line in lines if line]
-        shuffle(endpoints)
-        return endpoints
-
-    def decode_mds_dist_info(self, body):
-        obj = json.loads(body)
-        endpoints = [(x['host'], self.locator_port) for x in obj['primary']]
-        shuffle(endpoints)
-        return endpoints
-
-    @gen.coroutine
-    def fetch_mds_endpoints(self, request, srw_request):
         try:
             # NOTE: we can do it in a streaming way
             resp = yield self.srw_httpclient.fetch(srw_request)
             body = resp.buffer.read(None)
-            if self.is_stid_request(request):
-                raise gen.Return(self.decode_mulca_dist_info(body))
-            else:
-                raise gen.Return(self.decode_mds_dist_info(body))
+            lines = body.split("\n")
+            endpoints = [(line.split()[0], self.locator_port) for line in lines if line]
+            shuffle(endpoints)
+            locator = Locator(endpoints=endpoints)
+            app = Service(name, locator=locator, timeout=RESOLVE_TIMEOUT)
+            request.logger.info("connecting to app %s", name)
+            app = yield self.reelect_app(request, app)
+            yield self.proxy.process(request, name, app, event, pack_httprequest(request), self.reelect_app, timeout)
 
         except HTTPError as err:
             if err.code == 404:
